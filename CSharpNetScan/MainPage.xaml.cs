8c7ef3724143d99999722396f7c456a9eabca889
MarkAlexI <markovaleksandr108@gmail.com>
2024-06-04 00:13:55 +0300

    Added controls to main page.

--- CSharpNetScan/MainPage.xaml.cs
+++ CSharpNetScan/MainPage.xaml.cs
@@ -10,7 +10,26 @@ namespace CSharpNetScan
 		public MainPage()
 		{
 			InitializeComponent();
-			
+
+			entry.TextChanged += OnEntryTextChanged;
+			entry.Completed += OnEntryTextEntered;
+
+		}
+
+		private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+		{
+			string EnteredText = e.NewTextValue;
+			lbl_Input.Text = EnteredText;
+			Console.WriteLine(EnteredText);
+		}
+
+
+		private void OnEntryTextEntered(object sender, EventArgs e)
+		{
+			string uriString = entry.Text;
+                        Uri myUri = new Uri(uriString);
+			lbl_Input.Text = "https://metanit.com/";
+			Console.WriteLine(myUri);
 		}
 	}
 }
